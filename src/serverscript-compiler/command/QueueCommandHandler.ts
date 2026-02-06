e46f5416d683e83c0622a512885bf330aa8dd8b9
flenarn <smallmustard@gmail.com>
2026-02-06 10:58:03 +0100

    feat: `QueueCommandHandler`.

---
+++ src/serverscript-compiler/command/QueueCommandHandler.ts
@@ -0,0 +1,30 @@
+import { DynamicCommandHandler } from "../../runescript-compiler/configuration/command/DynamicCommandHandler";
+import { TypeCheckingContext } from "../../runescript-compiler/configuration/command/TypeCheckingContext";
+import { MetaType } from "../../runescript-compiler/type/MetaType";
+import { PrimitiveType } from "../../runescript-compiler/type/PrimitiveType";
+import { TupleType } from "../../runescript-compiler/type/TupleType";
+import { Type } from "../../runescript-compiler/type/Type";
+
+export class QueueCommandHandler implements DynamicCommandHandler {
+    private readonly queueType: InstanceType<typeof MetaType.Script>;
+    
+    constructor(queueType: Type) {
+        this.queueType = queueType as InstanceType<typeof MetaType.Script>;
+    }
+
+    typeCheck(context: TypeCheckingContext): void {
+        context.checkArgument(0, this.queueType);       // Script to queue.
+        context.checkArgument(1, PrimitiveType.INT);    // Delay before running script.
+        context.checkArgument(2, PrimitiveType.INT);    // Int arg to pass to script.
+
+        // TODO: (Type safety) Make sure queue script only expects up to 1 int arg (parameterType).
+        const expectedTypesList: Type[] = [
+            this.queueType,
+            PrimitiveType.INT,
+            PrimitiveType.INT
+        ];
+
+        context.checkArgumentTypes(TupleType.fromList(expectedTypesList));
+        context.expression.type = MetaType.Unit; 
+    }
+}
